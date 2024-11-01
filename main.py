b66d2a1860ad6f26a4da7b9631eb6bb532a0b0b0
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-11-01 13:11:32 -0500

    SolucionDivisores

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-#Escriba un programa que muestre una tabla de multiplicar como la siguiente:
+#Escriba un programa que entregue todos los divisores del número entero ingresado:
+#
+#Ingrese numero: 200
+#1 2 4 5 8 10 20 25 40 50 100 200
 
- #1   2   3   4   5   6   7   8   9  10
- #2   4   6   8  10  12  14  16  18  20
- #3   6   9  12  15  18  21  24  27  30
- #4   8  12  16  20  24  28  32  36  40
- #5  10  15  20  25  30  35  40  45  50
- #6  12  18  24  30  36  42  48  54  60
- #7  14  21  28  35  42  49  56  63  70
- #8  16  24  32  40  48  56  64  72  80
- #9  18  27  36  45  54  63  72  81  90
-#10  20  30  40  50  60  70  80  90 100
-#Los números deben estar alineados a la derecha.
+# Solicitar al usuario que ingrese un número entero
+numero = int(input("Ingrese número: "))
 
+# Inicializar una lista para almacenar los divisores
+divisores = []
 
+# Encontrar todos los divisores del número
+for i in range(1, numero + 1):
+    if numero % i == 0:
+        divisores.append(i)
 
-# Definir el tamaño de la tabla
-tamaño = 10
+# Mostrar los divisores
+print(" ".join(map(str, divisores)))
 
-# Imprimir el encabezado de la tabla
-for i in range(1, tamaño + 1):
-    print(f"{i:>3}", end=' ')
-print()
-
-# Imprimir las filas de la tabla
-for i in range(1, tamaño + 1):
-    for j in range(1, tamaño + 1):
-        print(f"{i * j:>3}", end=' ')
-    print()
