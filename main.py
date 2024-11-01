537ee7059b91728b7e6a126cefd8dedd0f83d606
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-11-01 13:03:26 -0500

    SolucionSumaEntreNumeros

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-#Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
+#Escriba un programa que pida al usuario dos números enteros, y luego entregue la suma de todos los números que están entre ellos. Por ejemplo, si los números son 1 y 7, 
+# debe entregar como resultado 2 + 3 + 4 + 5 + 6 = 20.
 
-#Ingrese num: 10
-#1 2 4 8 16 32 64 128 256 512 1024
+#Ingrese num: 1
+#Ingrese num: 7
+#La suma es 20
 
-# Solicitar al usuario que ingrese un número
-num = int(input("Ingrese num: "))
 
-# Generar y mostrar las potencias de 2
-for i in range(num + 1):
-    potencia = 2 ** i
-    print(potencia, end=' ')
+# Solicitar al usuario que ingrese dos números enteros
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
 
+# Asegurar que num1 sea el menor y num2 el mayor
+if num1 > num2:
+    num1, num2 = num2, num1
+
+# Calcular la suma de los números entre num1 y num2
+suma = sum(range(num1 + 1, num2))
+
+# Mostrar el resultado
+print(f"La suma es {suma}")
