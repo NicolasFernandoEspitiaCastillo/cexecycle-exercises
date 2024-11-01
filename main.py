2d7676fcc604005c7af8fdd06befa3f75fbf569e
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-11-01 13:15:46 -0500

    SolucionDibujoDeAsteriscos

--- main.py
+++ main.py
@@ -1,41 +1,63 @@
-#Un viajero desea saber cuánto tiempo tomó un viaje que realizó. Él tiene la duración en minutos de cada uno de los tramos del viaje.
+#Escriba un programa que pida al usuario ingresar la altura y el ancho de un rectángulo y lo dibuje utilizando asteriscos:
 
-#Desarrolle un programa que permita ingresar los tiempos de viaje de los tramos y entregue como resultado el tiempo total de viaje en formato horas:minutos.
+#Altura: 3
+#Ancho: 5
 
-#El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
+#*****
+#*****
+#*****
+#Escriba un programa que dibuje el triángulo del tamaño indicado por el usuario de acuerdo al ejemplo:
 
-#Duracion tramo: 15
-#Duracion tramo: 30
-#Duracion tramo: 87
-#Duracion tramo: 0
-#Tiempo total de viaje: 2:12 horas
-#Duracion tramo: 51
-#Duracion tramo: 17
-#Duracion tramo: 0
-#Tiempo total de viaje: 1:08 horas
+#Altura: 5
+
+#*
+#**
+#***
+#****
+#*****
+#Escriba un programa que dibuje el hexágono del tamaño indicado por el usuario de acuerdo al ejemplo:
+
+#Lado: 4
+
+#   ****
+#  ******
+# ********
+#**********
+# ********
+#  ******
+#   ****
 
 
-# Función para calcular el tiempo total en horas:minutos
-def calcular_tiempo_total():
-    total_minutos = 0
-    
-    while True:
-        # Solicitar la duración del tramo
-        duracion_tramo = int(input("Duración tramo: "))
-        
-        # Si el usuario ingresa 0, se termina la entrada
-        if duracion_tramo == 0:
-            break
-        
-        # Sumar la duración a los minutos totales
-        total_minutos += duracion_tramo
+#Programa para dibujar un rectangulo 
 
-    # Calcular horas y minutos
-    horas = total_minutos // 60
-    minutos = total_minutos % 60
-    
-    # Mostrar el tiempo total en formato horas:minutos
-    print(f"Tiempo total de viaje: {horas}:{minutos:02} horas")
+# Solicitar al usuario que ingrese la altura y el ancho del rectángulo
+altura = int(input("Altura: "))
+ancho = int(input("Ancho: "))
 
-# Llamar a la función
-calcular_tiempo_total()
+# Dibujar el rectángulo
+for _ in range(altura):
+    print('*' * ancho)
+
+
+#Programa para dibujar un triangulo 
+
+# Solicitar al usuario que ingrese la altura del triángulo
+altura = int(input("Altura: "))
+
+# Dibujar el triángulo
+for i in range(1, altura + 1):
+    print('*' * i)
+
+
+#Programa para dibujar un hexagono
+
+# Solicitar al usuario que ingrese el lado del hexágono
+lado = int(input("Lado: "))
+
+# Dibujar la parte superior del hexágono
+for i in range(lado):
+    print(' ' * (lado - i - 1) + '*' * (2 * i + 2))
+
+# Dibujar la parte inferior del hexágono
+for i in range(lado - 1):
+    print(' ' * (i + 1) + '*' * (2 * (lado - i - 1) + 2))
